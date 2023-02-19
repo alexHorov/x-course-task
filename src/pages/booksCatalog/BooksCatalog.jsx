43ff8cc2f50403a5c556bc645aa87668b0397ec4
Alexandr Horovyi <alexhorovyi@gmail.com>
2023-02-19 19:46:43 +0200

    delete comit

--- src/pages/booksCatalog/BooksCatalog.jsx
+++ src/pages/booksCatalog/BooksCatalog.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-// import { getAllBooks } from 'api.js';
 import { useUserInfo } from "hooks/useUserInfo";
 import { Spinner } from "components/spinner/Spinner";
 import { BooksList } from "components/booksList/BooksList";
@@ -15,7 +14,6 @@ function BooksCatalog() {
     e.preventDefault();
     setSearchValue(e.target.value);
   };
-  const price = "price";
 
   return (
     <main className="booksCard">
@@ -48,36 +46,6 @@ function BooksCatalog() {
           </select>
         </form>
       </section>
-
-      {/* <section className="input__field">
-          <form id="searchSection">
-            <div className="search">
-              <input
-                type="search"
-                id="searchTerm"
-                placeholder="Search by book name"
-                value={searchValue}
-                onChange={handleSearch}
-              />
-              <button type="submit" className="searchButton">
-                <i className="fa fa-search" />
-              </button>
-            </div>
-          </form>
-  
-          <form className="select-form">
-            <select
-              id="select-price"
-              value={filterValues}
-              onChange={(e) => setFilterValues(e.target.value)}
-            >
-              <option value="All">All</option>
-              <option value="15">less than 15$</option>
-              <option value="30">less than 30$</option>
-              <option value="1000">more than 30$</option>
-            </select>
-          </form>
-        </section> */}
 
       {!booksCatalog.length ? (
         <Spinner />
